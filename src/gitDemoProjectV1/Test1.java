422339d5cc79c842ecbafaee74d829447ca6196f
ankush29august <ankush29august@gmail.com>
2019-05-10 15:05:49 +0530

    first eclipse commit.......

---
+++ src/gitDemoProjectV1/Test1.java
@@ -0,0 +1,10 @@
+package gitDemoProjectV1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry="yes";
+		String res=(hungry=="yes")?"eat something":"Do work.....";
+		System.out.println(res);
+
+}
+}
